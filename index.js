423968cc30a8ed3acecb583fee22590429dd7a6d
ConstanceNwaigwe <constancenwaigwe@gmail.com>
2021-11-04 19:35:16 -0400

    added index.js file

---
+++ index.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+
+const express = require('express');
+const server = express();
+
+const port = process.env.PORT || 5000;
+const Users = [
+    {
+    id: 1,
+    name:"Jane Doe",
+    email:"janedoe@email.com"
+    },
+    {
+        id: 2,
+        name:"Amy Brown",
+        email:"Abrown@email.com"
+    }
+];
+
+server.get("/api/users", (req,res) => {
+    res.send(Users);
+})
+server.post("/api/register", (req,res) => {
+    Users.push(req.body);
+    res.status(201).json(req.body);
+})
+server.post("/api/login", (req,res) => {
+    Users.push(req.body);
+    res.status(201).json(req.body);
+})
+
+server.listen(port, () => {
+    console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
+  });
